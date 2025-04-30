555dd1195d3bfb7abe04fb3c27bdfd41bbc88d01
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-04-30 10:58:02 +0600

    Banner created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from '@/components/Navbar';
- import React from 'react';
+ import Banner from '@/components/Banner';
+import React from 'react';
 
 const page = () => {
   return (
     <div>
-      <Navbar />
-    </div>
+      <Banner/>
+     </div>
   );
 };
 
